1150a0afe2f3f72e826e36475c3b349d8909c447
Neelesh Bizoara <bizoaraneelesh@gmail.com>
2019-06-29 18:40:50 +0530

    React hooks: useState() method used in functional component but its not working righnow

--- react-complete-guide/src/Person/Another.js
+++ react-complete-guide/src/Person/Another.js
@@ -5,13 +5,33 @@ import React from 'react'
 import { useState } from 'react';
 
 const another = () => {
+    // Use useState for state in functional component
+    const [addressState, updateAddressState] = useState({
+        address: [
+            { addressLabel: 'Current Address', addressValue: 'Flat No 604, Yash Wisteria, Wakad, Pune' }
+        ]
+    });
+
+    const updateAddressHandler = () => {
+        updateAddressState({
+            address: [
+                { addressLabel: 'Permanent Address', addressValue: 'H.N. 54/445, Teachers Colony Danganya, Raipur, Chhattisgarh' }
+            ]
+        })
+    }
     // creatElement can take multiple parameters, but 3 argument is mandatry.
-    return React.createElement('div', null, React.createElement('h1', { className: 'another' }, 'Hi, I\'m React app!!!'));
+    // return React.createElement('div', null, React.createElement('h1', { className: 'another' }, 'Hi, I\'m React app!!!'));
+
     // The above code is equivalent to 
     // Note: class etc. can not be used in JSX as it is reserved keyword.
-    // return (<div>
-    //     <h1 className="another">Hi I\'m React app!!!</h1>
-    // </div>)
+    return (
+        <div>
+            <h4 className="another">Hi I'm functional component.</h4>
+            <button onClick={updateAddressHandler}>Get Permanant Address</button>
+            <div>{addressState.address.addressLabel}</div>
+            <div>{addressState.address.addressValue}</div>
+        </div>
+    )
 }
 
 export default another;
